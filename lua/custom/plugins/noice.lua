078bbc64c2640922df02d19f243790c9810357ab
GoldenGoosebert <ungureanuandreiflorin234@gmail.com>
2025-11-15 02:18:01 +0200

    modified lualine a bit

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -1,4 +1,4 @@
-return {
+  return {
   "folke/noice.nvim",
   event = "VeryLazy",
   dependencies = {
@@ -6,31 +6,19 @@ return {
     },
   config = function()
     require("noice").setup({
-    views = {
-      cmdline_popup = {
-        position = {
-          row = 5,
-          col = "50%",
-        },
-        border = {
-          style = "rounded",
-          padding = {0, 1},
-        },
+      cmdline = {
+        view = "cmdline",
       },
-      filter_options = {},
-      win_options = {
-        winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+      routes = {
+      {
+        filter = {
+          event = "msg_show",
+          kind = "",
+          find = "written",
+        }, 
       },
     },
-    opts = {},
-    ---@type table<string, CmdlineFormat>
-    format = {
-        cmdline = { pattern = "^:", icon = "", lang = "vim" },
-        search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
-        search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
-        filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
-        lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
-      }
+    opts = { skip = true},
   })
   end
 }
