ddcad89601ffa52b03b9d71222655a2a689164b8
GoldenGoosebert <ungureanuandreiflorin234@gmail.com>
2025-11-15 02:10:12 +0200

    changed some small stuff

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -1,20 +1,20 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = {'nvim-tree/nvim-web-devicons'},
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
 
-  config = function()
-    require('lualine').setup {
-  options = {
+config = function()
+  require('lualine').setup{
+   options = {
     icons_enabled = true,
     theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
     disabled_filetypes = {
-      statusline = {},
-      winbar = {},
+    statusline = {},
+    winbar = {},
     },
     ignore_focus = {},
-    always_divide_middle = true,
+    always_divide_middle = false,
     always_show_tabline = true,
     globalstatus = false,
     refresh = {
@@ -40,8 +40,8 @@ return {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
     lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
+    lualine_x = {'filetype'},
+    lualine_y = {},
     lualine_z = {'location'}
   },
   inactive_sections = {
@@ -56,7 +56,6 @@ return {
   winbar = {},
   inactive_winbar = {},
   extensions = {}
+        }
+end
 }
-  end
-}
-
